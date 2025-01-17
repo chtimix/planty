f2af05a657b25c3db67406459d403e8aa434f9f7
chtimix <david.peugnet77@gmail.com>
2025-01-17 04:35:06 +0100

    Mise à jour

---
+++ wp-content/themes/planty/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="wrapper" class="hfeed">
+        <header id="header" role="banner">
+            <div id="branding">
+                    <a href="http://<?php echo $_SERVER['HTTP_HOST'].'/planty';?>">
+                        <img src="http://<?php echo $_SERVER['HTTP_HOST'];?>/planty/wp-content/themes/planty/images/logo-planty.png" alt="Logo planty" >
+                    </a>
+            </div>
+            <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+            </nav>
+        </header>
+        <div id="container">
+            <main id="content" role="main">
