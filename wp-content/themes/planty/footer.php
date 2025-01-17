879038a8d64c5d3952cf3314838f9bb59c8f920d
chtimix <david.peugnet77@gmail.com>
2025-01-17 14:52:38 +0100

    Mise à jour

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,11 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+    <div>
+        <p><a href="#">Mentions légales</a></p>
+    </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/planty/page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="entry-content" itemprop="mainContentOfPage">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
+<?php the_content(); ?>
+</div>
+</article>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
