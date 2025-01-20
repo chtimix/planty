62baccc9e7c4ed16888c3c8bda521fe49a4770fd
chtimix <david.peugnet77@gmail.com>
2025-01-20 12:05:03 +0100

    Création page commande

--- wp-content/themes/planty/template-commander.php
+++ wp-content/themes/planty/template-commander.php
@@ -6,7 +6,6 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div id="page-commander" <?php post_class(); ?>>
-    <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
 </div>
 <?php endwhile; endif; ?>
